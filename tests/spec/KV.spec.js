231f608006fb3246f303488720c644f5897ceb67
George Borisov <git@gir.me.uk>
2018-09-02 22:01:37 +0100

    rework lib/KV.js and add lib/KVS.js

--- tests/spec/KV.spec.js
+++ tests/spec/KV.spec.js
@@ -4,114 +4,73 @@ const { expect } = require('chai');
 const KV = require('../../lib/KV.js');
 
 describe('lib/KV.js', function () {
-    let kv, v, w;
+    const logs = [];
+    let kv, v;
 
-    it('creates KV object with revision=0 if no initial value provided', function () {
-        kv = new KV('foo');
-        expect(kv.key).to.equal('foo');
-        expect(kv.revision).to.equal(0);
-        expect(kv.value).to.equal(undefined);
-    });
+    it('creates KV object', function () {
+        kv = new KV('foo', 'bar', function () {
+            logs.push(arguments);
+        });
 
-    it('creates KV object with revision=1 if initial value provided', function () {
-        kv = new KV('foo', 'bar');
-        expect(kv.key).to.equal('foo');
-        expect(kv.revision).to.equal(1);
-        expect(kv.value).to.equal('bar');
+        expect(kv.data.key).to.equal('foo');
+        expect(kv.data.revision).to.equal(0);
+        expect(kv.data.value).to.equal('bar');
+
+        expect(logs.length).to.equal(2);
+        expect(logs[0][0]).to.equal('debug');
+        expect(logs[0][1]).to.equal('Record created');
+        expect(logs[0][2].key).to.equal('foo');
+        expect(logs[0][2].revision).to.equal(0);
+        expect(logs[1][0]).to.equal('trace');
+        expect(logs[1][1]).to.equal('Record data');
+        expect(logs[1][2].data).to.equal('bar');
+        expect(logs[1][2].key).to.equal('foo');
+        expect(logs[1][2].revision).to.equal(0);
+
+        logs.splice(0, logs.length);
     });
 
     it('does not update simple type value if unchanged', function () {
         const updated = kv.set('bar');
 
         expect(updated).to.equal(false);
-        expect(kv.revision).to.equal(1);
-        expect(kv.value).to.equal('bar');
+        expect(kv.data.revision).to.equal(0);
+        expect(kv.data.value).to.equal('bar');
+
+        expect(logs.length).to.equal(0);
     });
 
     it('updates value', function () {
         const updated = kv.set(v = { bar: 'bar' });
 
         expect(updated).to.equal(true);
-        expect(kv.revision).to.equal(2);
-        expect(kv.value).to.equal(v);
+        expect(kv.data.revision).to.equal(1);
+        expect(kv.data.value).to.equal(v);
+
+        expect(logs.length).to.equal(2);
+        expect(logs[0][0]).to.equal('debug');
+        expect(logs[0][1]).to.equal('Record updated');
+        expect(logs[0][2].key).to.equal('foo');
+        expect(logs[0][2].revision).to.equal(1);
+        expect(logs[1][0]).to.equal('trace');
+        expect(logs[1][1]).to.equal('Record data');
+        expect(logs[1][2].data.bar).to.equal('bar');
+        expect(logs[1][2].key).to.equal('foo');
+        expect(logs[1][2].revision).to.equal(1);
+
+        logs.splice(0, logs.length);
     });
 
     it('always updates value of type object', function () {
-        v = { bar: 'bar' };
-        kv.set(v);
-        expect(kv.revision).to.equal(3);
-        expect(kv.value).to.equal(v);
-    });
+        kv.set(v = { bar: 'bar' });
 
-    it('watcher gets KV on initial subscription and update', function (done) {
-        let i = 0;
+        expect(kv.data.revision).to.equal(2);
+        expect(kv.data.value).to.equal(v);
 
-        w = function (kv) {
-            expect(kv.value).to.equal(v);
-            i += 1;
-        };
+        expect(logs.length).to.equal(2);
+        expect(logs[0][1]).to.equal('Record updated');
+        expect(logs[1][1]).to.equal('Record data');
 
-        kv.set(v = 'foo');
-        kv.on(undefined, w);
-
-        process.nextTick(function () {
-            expect(i).to.equal(1);
-            kv.set(v = 'bar');
-
-            process.nextTick(function () {
-                expect(i).to.equal(2);
-                done();
-            });
-        });
-    });
-
-    it('removes a watcher', function (done) {
-        kv.off(w);
-
-        for (let k of Reflect.ownKeys(kv)) {
-            if (typeof k === 'symbol' && k.toString() === 'Symbol(watchers)') {
-                expect(kv[k].size).to.equal(0);
-                return done();
-            }
-        }
-    });
-
-    it('watcher gets no initial KV if opts.noInitial is set, only updates', function (done) {
-        let i = 0;
-
-        w = function (kv) {
-            expect(kv.value).to.equal(v);
-            i += 1;
-        };
-
-        kv.on({ noInitial: true }, w);
-
-        process.nextTick(function () {
-            expect(i).to.equal(0);
-            kv.set(v = 'foo');
-
-            process.nextTick(function () {
-                expect(i).to.equal(1);
-                kv.off(w);
-                done();
-            });
-        });
-    });
-
-    it('watcher gets no initial KV if opts.haveRevision is >= revision number', function (done) {
-        let i = 0;
-
-        w = function (kv) {
-            expect(kv.value).to.equal(v);
-            i += 1;
-        };
-
-        kv.on({ haveRevision: kv.revision }, w);
-
-        process.nextTick(function () {
-            expect(i).to.equal(0);
-            kv.off(w);
-            done();
-        });
+        logs.splice(0, logs.length);
     });
 });
